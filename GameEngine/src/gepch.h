e3327ffa828aa1f8416004f713553d865111fd72
edwards.cole97@gmail.com <edwards.cole97@gmail.com>
2019-11-17 13:09:45 -0800

    added precompiled header

---
+++ GameEngine/src/gepch.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <memory>
+#include <iostream>
+#include <utility>
+#include <functional>
+#include <algorithm>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+
+#ifdef GE_PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif
